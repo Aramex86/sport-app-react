4200282e42576862a2302cd55691f53f91ffca71
Aramex86 <slavbez@mail.ru>
2020-07-04 00:02:08 +0300

    sidemenu animation

--- src/components/Header/Header-bottom/Navigation.js
+++ src/components/Header/Header-bottom/Navigation.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const Navigation = (props) => {
     return (
         <nav className="navigation-wrapp">
-            <ul className="nav-list">
+           <ul className="nav-list">
                 <li className="nav-list-item">
                     <NavLink to="/Sports Aceessories" className="nav-list-link">Sports Aceessories</NavLink>
                 </li>
@@ -20,7 +20,7 @@ const Navigation = (props) => {
                 <li className="nav-list-item">
                      <NavLink to="/Air Racing" className="nav-list-link">Blog</NavLink>
                 </li>
-            </ul>
+            </ul>   
         </nav>
     );
 };
--- src/components/Header/Header-bottom/Search-Bar/Nav-bar.js
+++ src/components/Header/Header-bottom/Search-Bar/Nav-bar.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import React from "react";
+import Icon from "../../../common/Icons";
+import { NavLink } from "react-router-dom";
 
-const NavBar = (props) => {
+class NavBar extends React.Component {
+    state={
+      onMenu:true
+    }
+
+    showMenu=()=>{
+      if(this.state.onMenu==false){
+        this.setState({
+          onMenu:true
+        });
+      }else{
+        this.setState({
+          onMenu:false
+        });
+      }
+    }
+     
+    
+
+  render() {
     return (
-        <div>
-            <div className="searchbar-nav">
-                <span className="searchbar-nav-name">Shop By Category</span>
-                <span className="line"></span>
-            </div>
+      <div>
+        <div className="searchbar__nav" onClick={this.showMenu}>
+          <span className="searchbar__nav-name" >Shop By Category</span>
+          <span className="line"></span>
+          <div className={this.state.onMenu?"hide":"searchbar__nav-menu"}>
+            <ul className="nav-list">
+              <li className="nav-list-item">
+                <NavLink to="/Sports Aceessories" className="nav-list-link">
+                  Sports Aceessories <Icon icon="chevron-down" size={10} />
+                </NavLink>
+              </li>
+              <li className="nav-list-item">
+                <NavLink to="/Biribol" className="nav-list-link">
+                  Biribol <Icon icon="chevron-down" size={10} />
+                </NavLink>
+              </li>
+              <li className="nav-list-item">
+                <NavLink to="/Badminton" className="nav-list-link">
+                  Badminton
+                </NavLink>
+              </li>
+              <li className="nav-list-item">
+                <NavLink to="/Air Racing" className="nav-list-link">
+                  Air Racing
+                </NavLink>
+              </li>
+              <li className="nav-list-item">
+                <NavLink to="/Air Racing" className="nav-list-link">
+                  Blog
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
     );
-};
+  }
+}
 
 export default NavBar;
 
