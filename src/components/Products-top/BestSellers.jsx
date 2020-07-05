1f4ff4bad4e4d3d23183d6e1d03c7090e446f226
Aramex86 <slavbez@mail.ru>
2020-07-06 00:25:49 +0300

    bestSellers random items

--- src/components/Products-top/BestSellers.jsx
+++ src/components/Products-top/BestSellers.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
 import ProductCard from "./ProductCard";
 
 export default function BestSellers(props) {
-    const product = props.products1.map((p,i) => (
-        <ProductCard
-          image={p.img}
-          name={p.name}
-          price={p.price}
-          discount={p.discount}
-          key={i}
-        />
-      ));
-    return <div className="bestsellers">{product}</div>
+  const newProd = [];
+
+  for (let i = 0; i < props.products1.length; i++) {
+    //console.log(props.products1[i]);
+    const random =props.products1[Math.floor(Math.random() * props.products1.length)];
+      newProd.push(random);
+    
+  }
+  console.log(newProd);
+
+  const product = newProd.map((p, i) => (
+    <ProductCard
+      id={p.id}
+      image={p.img}
+      name={p.name}
+      price={p.price}
+      discount={p.discount}
+      key={i}
+    />
+  ));
+  return <div className="bestsellers">{product}</div>;
 }
