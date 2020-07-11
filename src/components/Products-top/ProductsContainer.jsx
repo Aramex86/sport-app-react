8b4e949016b81d0d63092371d58f0035ec777b07
Aramex86 <slavbez@mail.ru>
2020-07-12 00:04:44 +0300

    switch components

--- src/components/Products-top/ProductsContainer.jsx
+++ src/components/Products-top/ProductsContainer.jsx
@@ -1,25 +1,63 @@
 import React, { Component } from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, Redirect } from "react-router-dom";
 import FeatureProducts from "./FeatureProducts";
 import BestSellers from "./BestSellers";
 import NewArrivals from "./NewArrivals";
 import { connect } from "react-redux";
 
 class ProductsContainer extends Component {
+
+  state={
+    f:false,
+    b:false,
+    n:false
+  }
+
+  showFeature=()=>{
+    if(this.state.f === false){
+      this.setState({
+        f:true,
+        b:false,
+        n:false
+      }) 
+    }
+  }
+  showBest=()=>{
+    if(this.state.b === false){
+      this.setState({
+        f:false,
+        b:true,
+        n:false
+      })
+    }
+  }
+  showNewArrivals=()=>{
+    if(this.state.n === false){
+      this.setState({
+        f:false,
+        b:false,
+        n:true
+      })
+    }
+  }
+
+
+
+ 
   render() {
     return (
       <section className="products__wrapp">
         <div className="products__wrapp-header">
           <nav className="products__wrapp-header-nav">
-            <Link to="/featureproducts" className="products__wrapp-header-link">
+            <button to="/featureproducts" className="products__wrapp-header-link" onClick={this.showFeature}>
               Feature Products
-            </Link>
-            <Link to="/bestsellers" className="products__wrapp-header-link">
+            </button>
+            <button to="/bestsellers" className="products__wrapp-header-link" onClick={this.showBest}>
               Best Sellers
-            </Link>
-            <Link to="/newarrivals" className="products__wrapp-header-link">
+            </button>
+            <button to="/newarrivals" className="products__wrapp-header-link" onClick={this.showNewArrivals}>
               New Arrivals
-            </Link>
+            </button>
           </nav>
 
           <div className="btn__wrapp">
@@ -27,17 +65,26 @@ class ProductsContainer extends Component {
             <button className="btn-next">next</button>
           </div>
         </div>
-        <Switch>
-          <Route
-            path="/featureproducts"
-            render={() => <FeatureProducts {...this.props} />}
-          />
-          <Route
-            path="/bestsellers"
-            render={() => <BestSellers {...this.props} />}
-          />
-          <Route path="/newarrivals" render={() => <NewArrivals {...this.props}/>} />
-        </Switch>
+        
+        {/* <Route
+          path="/featureproducts"
+          render={() => <FeatureProducts {...this.props} />}
+        />
+        <Route
+          path="/bestsellers"
+          render={() => <BestSellers {...this.props} />}
+        />
+        <Route
+          path="/newarrivals"
+          render={() => <NewArrivals {...this.props} />}
+        /> */}
+        {this.state.f?<FeatureProducts {...this.props}/>:<div></div>}            
+        {this.state.b?<BestSellers {...this.props}/>:<div></div>}            
+        {/* {this.state.b?<BestSellers {...this.props}/>:"b"}       
+        {this.state.n?<NewArrivals {...this.props}/>:"n"}        */}
+         {/* <FeatureProducts {...this.props}/> */}
+          {/* <BestSellers {...this.props}/>
+          <NewArrivals {...this.props}/> */}
       </section>
     );
   }
@@ -47,7 +94,7 @@ let mapStateToProps = (state) => {
   return {
     products: state.featureProd.products,
     products1: state.bestSellers.products1,
-    products2:state.newArrival.products2
+    products2: state.newArrival.products2,
   };
 };
 
--- src/components/Store/Products-top/newArrival-reducer.js
+++ src/components/Store/Products-top/newArrival-reducer.js
@@ -1,170 +1,164 @@
 //-----images-----//
-import boots from '../../../assets/products//baseball-boots.png';
-import gloves from '../../../assets/products/baseball-gloves.png';
-import helmet from '../../../assets/products/helmet.png';
-import nikebag from '../../../assets/products/nikebag.png';
-import nikehat from '../../../assets/products/nikehat.png';
-import ruksak from '../../../assets/products/ruksak.png';
-import socks from '../../../assets/products/socks.png';
-import shitki from '../../../assets/products/shitki.png';
-import skakalka from '../../../assets/products/skakalka.png';
-import sportbag from '../../../assets/products/sportbag.png';
-import sportgloves from '../../../assets/products/sportgloves.png';
-import sportshoes from '../../../assets/products/sportshoes.png';
-import sportshots from '../../../assets/products/sportshots.png';
-import sportsmart from '../../../assets/products/sportsmart.png';
-import sporttshirt from '../../../assets/products/sportt-shirt.png';
-import sportwatch1 from '../../../assets/products/sportwatch1.png';
-import taybox from '../../../assets/products/taybox.png';
+import boots from "../../../assets/products//baseball-boots.png";
+import gloves from "../../../assets/products/baseball-gloves.png";
+import helmet from "../../../assets/products/helmet.png";
+import nikebag from "../../../assets/products/nikebag.png";
+import nikehat from "../../../assets/products/nikehat.png";
+import ruksak from "../../../assets/products/ruksak.png";
+import socks from "../../../assets/products/socks.png";
+import shitki from "../../../assets/products/shitki.png";
+import skakalka from "../../../assets/products/skakalka.png";
+import sportbag from "../../../assets/products/sportbag.png";
+import sportgloves from "../../../assets/products/sportgloves.png";
+import sportshoes from "../../../assets/products/sportshoes.png";
+import sportshots from "../../../assets/products/sportshots.png";
+import sportsmart from "../../../assets/products/sportsmart.png";
+import sporttshirt from "../../../assets/products/sportt-shirt.png";
+import sportwatch1 from "../../../assets/products/sportwatch1.png";
+import taybox from "../../../assets/products/taybox.png";
 
 ////////////////////////
 
+let initialState = {
+  products2: [
+    {
+      id: 1,
+      name: "Nikehat",
+      deals: "Newest Arrivals",
+      img: `${nikehat}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 2,
+      name: "Backpack",
+      deals: "Newest Arrivals",
+      img: `${ruksak}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 3,
+      name: "Fotball Shields",
+      deals: "Newest Arrivals",
+      img: `${shitki}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 4,
+      name: "Baseball Gloves",
+      deals: "Limited Offer In",
+      img: `${gloves}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 5,
+      name: "Baseball Boots",
+      deals: "Best Deals In",
+      img: `${boots}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 6,
+      name: "Helmet",
+      deals: "Newest Arrivals",
+      img: `${helmet}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 7,
+      name: "Nikebag",
+      deals: "Newest Arrivals",
+      img: `${nikebag}`,
+      price: 29.99,
+    },
+    {
+      id: 8,
+      name: "Nikehat",
+      deals: "Newest Arrivals",
+      img: `${nikehat}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 9,
+      name: "Backpack",
+      deals: "Newest Arrivals",
+      img: `${ruksak}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 10,
+      name: "Fotball Shields",
+      deals: "Newest Arrivals",
+      img: `${shitki}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 11,
+      name: "jump Rope",
+      deals: "Newest Arrivals",
+      img: `${skakalka}`,
+      discount: 20,
+      price: 29.99,
+    },
+    {
+      id: 12,
+      name: "Sport Socks",
+      deals: "Newest Arrivals",
+      img: `${socks}`,
+      price: 29.99,
+    },
+    {
+      id: 13,
+      name: "Sportbag",
+      deals: "Newest Arrivals",
+      img: `${sportbag}`,
+      price: 29.99,
+    },
+    {
+      id: 14,
+      name: "Sport Gloves",
+      deals: "Newest Arrivals",
+      img: `${sportgloves}`,
+      price: 29.99,
+    },
+    {
+      id: 15,
+      name: "taybox",
+      deals: "Newest Arrivals",
+      img: `${taybox}`,
+      discount: 15,
+      price: 29.99,
+    },
+    {
+      id: 16,
+      name: "Sneakers",
+      deals: "Newest Arrivals",
+      img: `${sportshoes}`,
+      price: 29.99,
+    },
+    {
+      id: 17,
+      name: "Athletic Shorts",
+      deals: "Newest Arrivals",
+      img: `${sportshots}`,
+      price: 29.99,
+    },
+  ],
+};
 
-
-let initialState={
-    products2:[
-        {
-            id:1,
-            name:'Nikehat',
-            deals:'Newest Arrivals',
-            img:`${nikehat}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:2,
-            name:'Backpack',
-            deals:'Newest Arrivals',
-            img:`${ruksak}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:3,
-            name:'Fotball Shields',
-            deals:'Newest Arrivals',
-            img:`${shitki}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:4,
-            name:'Baseball Gloves',
-            deals:'Limited Offer In',
-            img:`${gloves}`,
-            discount:20,
-            price:29.99
-            
-        },
-        {
-            id:5,
-            name:'Baseball Boots',
-            deals:'Best Deals In',
-            img:`${boots}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:6,
-            name:'Helmet',
-            deals:'Newest Arrivals',
-            img:`${helmet}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:7,
-            name:'Nikebag',
-            deals:'Newest Arrivals',
-            img:`${nikebag}`,
-            price:29.99
-        },
-        {
-            id:8,
-            name:'Nikehat',
-            deals:'Newest Arrivals',
-            img:`${nikehat}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:9,
-            name:'Backpack',
-            deals:'Newest Arrivals',
-            img:`${ruksak}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:10,
-            name:'Fotball Shields',
-            deals:'Newest Arrivals',
-            img:`${shitki}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:11,
-            name:'jump Rope',
-            deals:'Newest Arrivals',
-            img:`${skakalka}`,
-            discount:20,
-            price:29.99
-        },
-        {
-            id:12,
-            name:'Sport Socks',
-            deals:'Newest Arrivals',
-            img:`${socks}`,
-            price:29.99
-        },
-        {
-            id:13,
-            name:'Sportbag',
-            deals:'Newest Arrivals',
-            img:`${sportbag}`,
-            price:29.99
-        },
-        {
-            id:14,
-            name:'Sport Gloves',
-            deals:'Newest Arrivals',
-            img:`${sportgloves}`,
-            price:29.99
-        },
-        {
-            id:15,
-            name:'taybox',
-            deals:'Newest Arrivals',
-            img:`${taybox}`,
-            discount:15,
-            price:29.99
-        },
-        {
-            id:16,
-            name:'Sneakers',
-            deals:'Newest Arrivals',
-            img:`${sportshoes}`,
-            price:29.99
-        },
-        {
-            id:17,
-            name:'Athletic Shorts',
-            deals:'Newest Arrivals',
-            img:`${sportshots}`,
-            price:29.99
-        }
-    ]
-}
-
-
-const newArrival=(state=initialState,action)=>{
-    switch(action.type){
-        default:
-            return state;
-    }
-}
-
-
+const newArrival = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
 export default newArrival;
